c4ef70d51ff36880fa85e566c45599df0575e103
William Easton <bill.easton@elastic.co>
2025-05-31 13:57:23 -0500

    remove proxy mcp

--- src/fastmcp/contrib/tool_transformer/loader.py
+++ src/fastmcp/contrib/tool_transformer/loader.py
@@ -4,11 +4,7 @@ from typing import Any
 
 import yaml
 
-from fastmcp.client.client import Client
 from fastmcp.contrib.tool_transformer.models import ToolOverride
-from fastmcp.contrib.tool_transformer.tool_transformer import proxy_tool
-from fastmcp.server.server import FastMCP
-from fastmcp.utilities.mcp_config import MCPConfig
 
 
 def overrides_from_dict(obj: dict[str, Any]) -> dict[str, ToolOverride]:
@@ -34,23 +30,3 @@ def overrides_from_json(json_str: str) -> dict[str, ToolOverride]:
 def overrides_from_json_file(json_file: Path) -> dict[str, ToolOverride]:
     with Path(json_file).open(encoding="utf-8") as f:
         return overrides_from_json(f.read())
-
-
-async def proxy_mcp_server_with_overrides(
-    config: MCPConfig,
-    tool_override: ToolOverride,
-    client_kwargs: dict[str, Any] = {},
-    server_kwargs: dict[str, Any] = {},
-) -> FastMCP:
-    """Run a MCP server with overrides."""
-
-    client = Client(config, **client_kwargs)
-
-    server = FastMCP.as_proxy(client, **server_kwargs)
-
-    server_tools = await server.get_tools()
-
-    for tool in server_tools.values():
-        proxy_tool(tool, server, override=tool_override)
-
-    return server
